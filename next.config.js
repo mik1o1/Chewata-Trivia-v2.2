a0548ff880e659714d5e0e2fbd4625dbe294be5f
Mik1o1 <98643303+mik1o1@users.noreply.github.com>
2024-10-06 18:34:47 +0300

    Revert "skip linting"
    This reverts commit b2bcb9352b1f971f3d41173731e29becabcc3268.

--- next.config.js
+++ next.config.js
@@ -3,10 +3,4 @@ const nextConfig = {
 	reactStrictMode: false
 }
 
-module.exports = {
-	eslint: {
-	  // Warning: This allows production builds to successfully complete even if
-	  // your project has ESLint errors.
-	  ignoreDuringBuilds: true,
-	},
-  }
+module.exports = nextConfig
