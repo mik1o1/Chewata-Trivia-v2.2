5b2323d1d2b745190d55219ab19e755791fed48c
Mik1o1 <98643303+mik1o1@users.noreply.github.com>
2024-10-06 18:09:34 +0300

    Revert "changes on font"
    This reverts commit 82e9fe97ace0d4604a34d5b32e7618fc124585f6.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,6 @@
 import '@/styles/globals.css'
-import { Rubik } from '@next/font/google'
 import Head from 'next/head'
 import NewGameForm from '@/components/Form/NewGameForm'
-const rubik = Rubik({ subsets: ['latin'] })
 
 export default function App ({ Component, pageProps }) {
 	return (
@@ -12,11 +10,7 @@ export default function App ({ Component, pageProps }) {
 			</Head>
 			<Component {...pageProps} />
 			<NewGameForm />
-			<style jsx global>{`
-        html {
-          font-family: ${rubik.style.fontFamily};
-        }
-      `}</style>
+			
 		</>
 	)
 }
