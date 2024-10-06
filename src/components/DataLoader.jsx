f9be6b625848b05624bb6f2f11a2084c40880994
Mik1o1 <98643303+mik1o1@users.noreply.github.com>
2024-10-06 03:03:08 +0300

    rule 3

--- src/components/DataLoader.jsx
+++ src/components/DataLoader.jsx
@@ -32,7 +32,7 @@ export default function DataLoader() {
     }, []); // Only run on component mount
 
     return (
-        <div classname="text-right">
+        <div className="text-right">
             {loading && <p >Loading</p>}
             {error && <p>🟡</p>}
             {!loading && !error && <p>🟢</p>}
